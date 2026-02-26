b14ee8f3b50820f509e2913f24e6e66688ab08ff
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-02-26 10:58:12 +0600

    new nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,17 @@
-'use client'
+'use client';
+
 export default function Navbar() {
   return (
-    <nav>
-      <h1>Navbar</h1>
-    </nav>
-  )
+    <>
+      <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div className="flex items-center cursor-pointer">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap">
+              Ecommercery
+            </span>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
