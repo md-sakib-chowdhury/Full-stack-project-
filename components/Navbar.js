9852aaf79038c77855fffc0d8081d880184ea1f1
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-02-26 12:04:14 +0600

    new commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,72 @@
+// 'use client';
+
+// import { Fragment } from 'react';
+
+// const isAdminView = true;
+// const isAuthUser = false;
+// const user = {
+//   role: 'admin'
+// }
+// function NavItems() {
+//   return (
+//     <div
+//       className="items-center justify-between w-full md:flex md:w-auto"
+//       id="nav-items"
+//     >
+//       <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white">
+//         {
+//           isAdminView ? adminNavOptions.map(item => <li className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0" key={item.id}
+//           >
+//             {item.lable}
+//           </li>) : null
+//         }
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default function Navbar() {
+//   return (
+//     <>
+//       <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+//         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+//           <div className="flex items-center cursor-pointer">
+//             <span className="self-center text-2xl font-semibold whitespace-nowrap">
+//               Ecommercery
+//             </span>
+//           </div>
+//           <div className="flex md:order-2 gap-2">
+//             {!isAdminView && isAuthUser ? (
+//               <Fragment>
+//                 <button>Account</button>
+//                 <button>Cart</button>
+//               </Fragment>
+//             ) : null}
+
+//             {user?.role === 'admin' ?
+//               isAdminView ? <button>Client view</button> : <button>Admin view</button>
+//               : null}
+//             {
+//               isAuthUser ? <button>Logout</button> : <button>Login</button>
+//             }
+//           </div>
+//           <NavItems />
+//         </div>
+//       </nav>
+//     </>
+//   );
+// }
 'use client';
 
 import { Fragment } from 'react';
+import { adminNavOptions, navOptions } from '@/utils';
 
-const isAdminView = false;
-const isAuthUser = true;
+const isAdminView = true;
+const isAuthUser = false;
 const user = {
   role: 'admin'
 }
+
 function NavItems() {
   return (
     <div
@@ -15,7 +75,15 @@ function NavItems() {
     >
       <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white">
         {
-          
+          isAdminView ? adminNavOptions.map(item => (
+            <li className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0" key={item.id}>
+              {item.label}
+            </li>
+          )) : navOptions.map(item => (
+            <li className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0" key={item.id}>
+              {item.label}
+            </li>
+          ))
         }
       </ul>
     </div>
@@ -47,6 +115,7 @@ export default function Navbar() {
               isAuthUser ? <button>Logout</button> : <button>Login</button>
             }
           </div>
+          <NavItems />
         </div>
       </nav>
     </>
